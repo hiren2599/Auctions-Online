7858929da8ac4396af5a56724b4511f969345bb2
hiren <hirenlineshbhaipatel@gmail.com>
2024-07-17 11:24:53 +0530

    chore(allowed-host): Updating Allowed hosts

--- auctionsonline/settings.py
+++ auctionsonline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5evyjl9v(s*#upwkq^e@w(cng!5cktbl!e5@8vws2!b53y3!85'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'hlp2599.pythonanywhere.com', '127.0.0.1' ]
 
 
 # Application definition
